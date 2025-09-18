65d4a82175dd8798126a31f1c2669f2ef7d0dec4
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2025-09-18 13:02:25 +0100

    refactor(prettier): added format to code

--- src/extension.ts
+++ src/extension.ts
@@ -1,78 +1,70 @@
 // import * as vscode from "vscode";
-import * as fs from "fs";
-import * as vscode from "vscode";
-import { createFolders } from "./commands/create_folders";
-import { createUsecase, getTemplatesFile } from "./commands/create_usecase";
-import { createInitials } from "./commands/create_initials";
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+import { createFolders } from './commands/create_folders';
+import { createUsecase, getTemplatesFile } from './commands/create_usecase';
+import { createInitials } from './commands/create_initials';
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand(
-    "tdd-clean-architecture.createFolders",
+    'tdd-clean-architecture.createFolders',
     async (uri: vscode.Uri) => {
       await createFolders(uri);
-    }
+    },
   );
 
   vscode.commands.registerCommand(
-    "tdd-clean-architecture.createUsecase",
+    'tdd-clean-architecture.createUsecase',
     async (uri: vscode.Uri) => {
       if (await templatesOk(uri)) {
         await createUsecase(uri);
       } else {
         const YES_NO = await vscode.window.showWarningMessage(
           "The '.my_templates' folder was not found!\nDo you want to download some default templates as an example?\nPS: You can create your own examples using them as a base.",
-          "Yes",
-          "No"
+          'Yes',
+          'No',
         );
-        if (YES_NO === "Yes") {
+        if (YES_NO === 'Yes') {
           getTemplatesFile(uri);
         } else {
           vscode.window.showInformationMessage(
-            `Tip: Look for templates in github:\nhttps://github.com/euclidesgc/clean-architecture-scaffolding/tree/main/.my_templates`
+            `Tip: Look for templates in github:\nhttps://github.com/alcampospalacios/flutter_tdd_clean_architecture/tree/main/.my_templates/flutter_tdd_clean_templates`,
           );
 
-          vscode.window.showErrorMessage(
-            `It is not possible to continue without templates!`
-          );
+          vscode.window.showErrorMessage(`It is not possible to continue without templates!`);
         }
       }
-    }
+    },
   );
 
   vscode.commands.registerCommand(
-    "tdd-clean-architecture.createInitials",
+    'tdd-clean-architecture.createInitials',
     async (uri: vscode.Uri) => {
       // Use the new createInitials function that handles all template files
       await createInitials(uri);
-    
-    }
+    },
   );
 
   async function templatesOk(uri: vscode.Uri): Promise<Boolean> {
     const folderList = (await vscode.workspace
-      .getConfiguration("scaffolding")
-      .get("layers.templates")) as Array<string>;
+      .getConfiguration('scaffolding')
+      .get('layers.templates')) as Array<string>;
 
-    const templateList = folderList.filter((element) =>
-      element.endsWith(".template")
-    );
+    const templateList = folderList.filter((element) => element.endsWith('.template'));
 
     if (folderList.length <= 0 && templateList.length <= 0) {
       return false;
     }
-    
-    const rootFolder = uri.path.substring(1, uri.path.indexOf("/lib"));
-    let fileName = "";
+
+    const rootFolder = uri.path.substring(1, uri.path.indexOf('/lib'));
+    let fileName = '';
 
     if (templateList && templateList.length > 0) {
       for (const key in templateList) {
         if (Object.prototype.hasOwnProperty.call(templateList, key)) {
           const element = templateList[key];
 
-          let fileName = element.substring(
-            element.lastIndexOf("/") + 1,
-            element.length
-          );
+          let fileName = element.substring(element.lastIndexOf('/') + 1, element.length);
 
           const templateFile = `${rootFolder}/.my_templates/${fileName}`;
           if (!fs.existsSync(templateFile)) {
@@ -83,7 +75,7 @@ export function activate(context: vscode.ExtensionContext) {
     } else {
       return false;
     }
-    
+
     return true;
   }
 }
